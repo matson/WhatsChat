97ce978eb5ba8a31f211e0fd041efd25c2e99de7
Madison <madison.k.adams@gmail.com>
2023-05-31 12:34:51 -0400

    starting files

---
+++ WhatsChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsChat
+//
+//  Created by Tracy Adams on 5/31/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
