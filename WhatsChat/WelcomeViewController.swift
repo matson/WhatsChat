55002ddd476e5e3ea83b4563d3bb919c92406b88
Madison <madison.k.adams@gmail.com>
2023-05-31 15:06:34 -0400

    controllers and more

--- WhatsChat/WelcomeViewController.swift
+++ WhatsChat/WelcomeViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var registerButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
