7bae6411a63d664b345c6178fc3eaad32ded5d4c
Madison <madison.k.adams@gmail.com>
2023-05-31 15:31:36 -0400

    navigation controller stuff

--- WhatsChat/WelcomeViewController.swift
+++ WhatsChat/WelcomeViewController.swift
@@ -13,12 +13,32 @@ class WelcomeViewController: UIViewController {
     
     @IBOutlet weak var registerButton: UIButton!
     
-    
+    @IBOutlet weak var whatsChat: UILabel!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let newNavBarAppearance = UINavigationBarAppearance()
+
+        // Configuring an opaque background
+//        newNavBarAppearance.configureWithOpaqueBackground()
+        // or you can use:
+         newNavBarAppearance.configureWithTransparentBackground()
+        // when you wanted a configuration with a transparent background
+        
+        // Navigation bar's background color
+        newNavBarAppearance.backgroundColor = .magenta
+        
+        // Navigation bar's title foreground color
+        newNavBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        // Apply the appearance to different styles:
+        navigationController?.navigationBar.scrollEdgeAppearance = newNavBarAppearance
+        navigationController?.navigationBar.compactAppearance = newNavBarAppearance
+        navigationController?.navigationBar.standardAppearance = newNavBarAppearance
+        if #available(iOS 15.0, *) {
+            navigationController?.navigationBar.compactScrollEdgeAppearance = newNavBarAppearance
+        }
     }
     
     @IBAction func registerPressed(_ sender: UIButton) {
